a4fa0eefa5879b5cfe8b961ae71b1b48317ea525
Bishal Biswas <bbiswas47@outlook.com>
2024-05-14 13:10:26 +0530

    Homepage Design

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,16 @@
+
+/*For Back to top starts*/
+window.addEventListener('scroll', function () {
+    // mybutton = document.getElementById("BackToTop");
+    if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 10) {
+        /* mybutton.classList.add("opacity-100");
+        mybutton.classList.remove("opacity-0");   */      
+        this.document.querySelector("header").classList.add("bg-[#000a229a]");
+        this.document.querySelector("header").classList.add("backdrop-blur-sm");
+    } else {
+       /*  mybutton.classList.remove("opacity-100");
+        mybutton.classList.add("opacity-0"); */
+        this.document.querySelector("header").classList.remove("bg-[#000a229a]");
+        this.document.querySelector("header").classList.remove("backdrop-blur-sm");
+    }        
+})
---
+++ src/pages/data/siteData.js
@@ -0,0 +1,6 @@
+var data = {
+    "siteTitle" : "CSF SERVICES",
+    "Content" : ""
+}
+
+export default data;
