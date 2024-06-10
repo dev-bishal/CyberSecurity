89be942f4076b155b03040cc95dbc1634c787e95
Bishal Biswas <bbiswas47@outlook.com>
2024-06-10 20:47:10 +0530

    SiteData.js file added for Content Management

--- src/pages/data/siteData.js
+++ src/pages/data/siteData.js
@@ -1,6 +1,303 @@
 var data = {
-    "siteTitle" : "CSF SERVICES",
-    "Content" : ""
+    "siteTitle": "CyberSecurity Finland | CSF SERVICES",
+    "MetaDescription": "Be Secured against Insider Threats and Cyber Security Risks within your Organization",
+
+    "textContent":
+    {
+        HomePage:
+        {
+            top: {
+                topLink: {
+                    title: "Experiencing a Cyberattack? Get help now.",
+                    link: "javascript:void(0);"
+                },
+                MainHeading: "The More companies trust Sophos for MDR than any other cybersecurity provider.",
+                CallToActionLinks : {
+                    videoPlayButton : {
+                        title : "See How",
+                        redirectLink : "javascript:void(0)",
+                    },
+                    learnMoreButton : {
+                        title : "Learn More",
+                        redirectLink : "javascript:void(0)",
+                    },
+                    textLink : {
+                        title : "Speak with an expert ",
+                        redirectLink : "javascript:void(0)",
+                    },
+                },
+                flipCards: [
+                    {
+                        iconClass: "fa-light fa-circle-exclamation-check",
+                        frontHeadingTitle: "Instant Security Operations Center",
+                        frontContent: "Achieve superior cybersecurity outcomes through a fully-managed MDR service or self-managed security operations platform.",
+                        backTitle: "Instant Security Operations Center",
+                        backContent: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa optio omnis, quasi odit ipsa, nisi tenetur provident adipisci vel quos libero ut. Delectus fugit atque repudiandae beatae, odio libero expedita?",
+                        learnMoreLink: "javascript:void(0)"
+                    },
+                    {
+                        iconClass: "fa-light fa-circle-exclamation-check",
+                        frontHeadingTitle: "Instant Security Operations Center",
+                        frontContent: "Achieve superior cybersecurity outcomes through a fully-managed MDR service or self-managed security operations platform.",
+                        backTitle: "Instant Security Operations Center",
+                        backContent: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa optio omnis, quasi odit ipsa, nisi tenetur provident adipisci vel quos libero ut. Delectus fugit atque repudiandae beatae, odio libero expedita?",
+                    },
+                    {
+                        iconClass: "fa-light fa-circle-exclamation-check",
+                        frontHeadingTitle: "Instant Security Operations Center",
+                        frontContent: "Achieve superior cybersecurity outcomes through a fully-managed MDR service or self-managed security operations platform.",
+                        backTitle: "Instant Security Operations Center",
+                        backContent: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa optio omnis, quasi odit ipsa, nisi tenetur provident adipisci vel quos libero ut. Delectus fugit atque repudiandae beatae, odio libero expedita?",
+                        learnMoreLink: "javascript:void(0)"
+                    },
+                ]
+            },
+            Services: {
+                heading: "Identities & Access, Intelligently Secured",
+                textParagraphs: [
+                    "Identities and their privileges are at the heart of every attack, and identity threats are hard to detect and increasingly complex. But complexity shouldn't mean compromise.",
+                    "BeyondTrust is the only identity security platform that allows you to detect threats across your entire identity estate and respond by controlling privileges, access, credentials, and secrets.",
+                    "With seamless integrations, unmatched discovery, and an identity-first data lake, we enable a zero-trust approach to least privilege, plus intelligent threat detection, to continuously strengthen your identity security posture.",
+                ],
+                redirectLink: {
+                    linkTitle: "See the Power of the Platform",
+                    linkURL: "javascript:void(0)"
+                }
+            },
+            Expertise: {
+                title: "Expertise",
+                HeadingLine1: "Our Area",
+                HeadingLine2: "of Expertise",
+                textParagraph: "We offer a range of strategy and implementation services to help you navigate the technology and cyber security landscape with confidence and efficiency.",
+
+                rightSide_Paragraphs: [
+                    {
+                        heading: "Assess",
+                        textParagraph: "LinearStack provides cybersecurity services, tackling ransomware and breaches. They offer assessments, penetration testing, purple teaming, emergency response training, and endpoint vulnerability evaluations.",
+                        iconFile: "internet-security.svg",
+                        redirectLink: {
+                            title: "Learn More",
+                            URL: "javascript:void(0)"
+                        }
+                    },
+                    {
+                        heading: "Manage",
+                        textParagraph: "LinearStack offers advanced security solutions: firewall, URL filtering, secure gateways, DDoS protection, DNS security, and SIEM management, ensuring comprehensive protection against cyber threats.",
+                        iconFile: "infrastructure.svg",
+                        redirectLink: {
+                            title: "Learn More",
+                            URL: "javascript:void(0)"
+                        }
+                    },
+                    {
+                        heading: "Respond",
+                        textParagraph: "LinearStack's services include Managed Detection & Response, proactive threat hunting, and 24x7 cyber vigilance. They offer specialized responses for incidents, breaches, cloud events, and phishing attacks, complemented by expert malware analysis to safeguard against evolving cyber threats.",
+                        iconFile: "digital-transformation.svg",
+                        redirectLink: {
+                            title: "Learn More",
+                            URL: "javascript:void(0)"
+                        }
+                    }
+                ]
+            },
+            Incident: {
+                heading: "The benefits of incident response",
+                textParagraph: "CyberSecurity's IR approach combines the industry-leading CyberSecurity Finland® platform along with an experienced team of responders to provide the following",
+                rightSide_Paragraphs: [
+                    {
+                        title: "Speed and efficacy",
+                        textParagraph: "CrowdStrike consultants’ skills and experience, combined with proven methodology and technology, allow the team to respond and contain incidents faster and more efficiently. The result: fewer hours incurred, less business interruption and lower costs to you."
+                    },
+                    {
+                        title: "Intelligence-led investigation",
+                        textParagraph: "The IR team is supported throughout the response by the CrowdStrike Intelligence team. As a pioneer in adversary analysis, it helps identify adversaries present in the environment, enabling the IR team to quickly and efficiently contain the incident."
+                    },
+                    {
+                        title: "Tailored approach",
+                        textParagraph: "CrowdStrike partners with you to develop a plan that takes into consideration your operational needs, as well as existing investments and resources, ensuring a thorough investigation and a customized remediation action plan."
+                    },
+                    {
+                        title: "Tools and technology",
+                        textParagraph: "The same Falcon endpoint technology, cyber threat intelligence and proactive managed hunting services used in the IR investigation are available to you in the future, ensuring that you can both improve your security posture and stop future breaches."
+                    }
+                ],
+            },
+            ContactCards: {
+                cardsContent: [
+                    {
+                        iconFile: "icon-paperclip.svg",
+                        title: "Partner with us",
+                        textParagraph: "Leverage the first cloud-based security operations platform to give your clients world-class cybersecurity services.",
+                        redirectLink: {
+                            title: "Learn More",
+                            URL: "javascript:void(0)"
+                        }
+                    },
+                    {
+                        iconFile: "icon-users.svg",
+                        title: "Join our team",
+                        textParagraph: "Join us in a journey of innovation and discovery, as we shape the future of cybersecurity operations.",
+                        redirectLink: {
+                            title: "See openings",
+                            URL: "javascript:void(0)"
+                        }
+                    },
+                    {
+                        iconFile: "icon-envelope.svg",
+                        title: "Contact Us",
+                        textParagraph: "Ready to speak to a human? feedback? We're always on a quest to improve so please share your thoughts.",
+                        redirectLink: {
+                            title: "Contact Us",
+                            URL: "javascript:void(0)"
+                        }
+                    }
+                ]
+            },
+            TrustedBrands : {
+                heading : "Trusted by the Best",
+                brands : [
+                    "PAN-Customer-callouts-07_ADT",
+                    "PAN-Customer-callouts-07_AutoDesk",
+                    "PAN-Customer-callouts-07_Better-mortgage",
+                    "PAN-Customer-callouts-07_Caesar",
+                    "PAN-Customer-callouts-07_Entel-telefonia",
+                    "PAN-Customer-callouts-07_Envision-racing",
+                    "PAN-Customer-callouts-07_Grupo-bimbo",
+                    "PAN-Customer-callouts-07_Michelin",
+                    "PAN-Customer-callouts-07_Resolution-life",
+                    "PAN-Customer-callouts-07_Salesforce",
+                    "PAN-Customer-callouts-07_Zespri"
+                    ]
+            },
+            VideoBgText: {
+                textContent : {
+                    slide1_Text1: "Your threat landscape",
+                    slide1_Text2: "is always evolving.",
+                    slide2_Text1: "Sophisticated threats",
+                    slide2_Text2: "dominate today's headlines.",
+                    slide3_Text1: "Together",
+                    slide3_Text2: ", we can protect",
+                    slide3_Text3: "against",
+                    slide3_Text4: "what's next.",
+                }
+            },
+            SliderCards : {
+                heading: "Cyber Security Finland",
+                heading2 : "Insights", 
+                textParagraph : "The latest research and expert perspectives",
+                sliderCardsContent : [
+                    {
+                        cardTitle : "See Why Customers Choose Us",
+                        cardTextContent : "Check out an easy-to-scan comparison of how we outperform the competition on multiple fronts",
+                        cardRedirectTitle : "Learn More",
+                        cardRedirectLink : "javascript:void(0)"
+                    },
+                    {
+                        cardTitle : "Building Resilience: 2024 Security Predictions for the Cloud",
+                        cardTextContent : "A look into stealthy dangers of data poisoning in machine learning models, the complexities of securing APIs, and more",
+                        cardRedirectTitle : "Read Blog",
+                        cardRedirectLink : "javascript:void(0)"
+                    },
+                    {
+                        cardTitle : "Diving Deep Into Quantum Computing",
+                        cardTextContent : "An exploration of computing with quantum mechanics",
+                        cardRedirectTitle : "Learn More",
+                        cardRedirectLink : "javascript:void(0)"
+                    },
+                    {
+                        cardTitle : "The Dynamic DoS Threat",
+                        cardTextContent : "Insights into the complex cyber landscape",
+                        cardRedirectTitle : "Read More",
+                        cardRedirectLink : "javascript:void(0)"
+                    }
+                ]
+            },
+            ExpandCards:{
+                title: "Solution by role",
+                heading: "Stay ahead of any security challenge",
+                hoverCards : [
+                    {
+                        iconFile : "ciso-icn.svg",
+                        cardTitle : "CISO",
+                        cardTextContent : "Security excellence is within reach. Discover market-leading XDR, dynamic ASRM, and purpose-build solutions to meet your needs today and tomorrow.",
+                        cardRedirectText : "Learn More",
+                        cardRedirectLink : ""
+                    },
+                    {
+                        iconFile : "soc-manager-icn.svg",
+                        cardTitle : "SOC Managers",
+                        cardTextContent : "Overcome siloes, complexity, and your growing attack surface to stop adversaries faster with a unified cybersecurity platform.",
+                        cardRedirectText : "Learn More",
+                        cardRedirectLink : ""
+                    },
+                    {
+                        iconFile : "cloud-secops-icn.svg",
+                        cardTitle : "Cloud SecOps",
+                        cardTextContent : "Gain full visibility and control to resolve multi-cloud complexity, so you can achieve compliance and secure cloud-native apps.",
+                        cardRedirectText : "Learn More",
+                        cardRedirectLink : ""
+                    },
+                    {
+                        iconFile : "cloud-developer-icn.svg",
+                        cardTitle : "Cloud Developer",
+                        cardTextContent : "Balance security asks while delivering innovative, on-time apps so you can build securely—your way.",
+                        cardRedirectText : "Learn More",
+                        cardRedirectLink : ""
+                    },
+                    {
+                        iconFile : "infrastructure-manager-icn.svg",
+                        cardTitle : "Infrastructure Managers",
+                        cardTextContent : "Evolve your security to protect more with less, enabling you to mitigate threats quickly and effectively.",
+                        cardRedirectText : "See how",
+                        cardRedirectLink : ""
+                    }
+                ]
+            }
+        },
+        Services1 : {
+            header : {
+                heading : "Our Services",
+                textParagraph : "Protect your digital assets with our comprehensive cybersecurity services, offering robust solutions including threat detection, risk assessment, and incident response to safeguard your business against evolving cyber threats.",
+            },
+            servicesGrid : {
+                title : "Optimize your security solutions",
+                textContent1 : "CyberSecurity Finland Services help you gain a deeper, broader use of your CyberSecurity technologies and products, built around every stage of the security lifecycle. Our experienced consultants and experts help you realize the full value of your security solutions and avoid the challenges that can lead to unexpected risks. We help ensure your security products are effectively deployed, integrated, assessed, and optimized in alignment with your security strategy.",
+                textContent2 : "You need the right talent at the right time. Let us help you integrate your security products and align your security strategy to reach new levels of performance.",
+                rightCardText : "Continuous monitoring of networks, systems, and applications to identify and respond to security threats in real time."
+            },
+            awardsSection:{
+                title: "Industry recognition",
+                subText: "CyberSecurity Finland is recognized as an industry leader by key analyst firms",
+                awardCards : [
+                    {
+                        awardTitle : "Editor's Choice for Extended Detection and Response (XDR) in 2023",
+                        imageFile : "https://www.trellix.com/en-us/img/logos/top-infosec-innovator-winner-2023-badge.png",
+                        redirectPath : "javascript:void(0)",
+                    },
+                    {
+                        awardTitle : "2023 Cybersecurity Excellence Awards Gold Winner Endpoint Security (NA)",
+                        imageFile : "https://www.trellix.com/en-us/img/logos/cybersecurity-award-2023-winner-gold.png",
+                        redirectPath : "javascript:void(0)",
+                    },
+                    {
+                        awardTitle : "SE Labs awards Trellix Email Security AAA and 100% Total Accuracy Ratings",
+                        imageFile : "https://www.trellix.com/en-us/img/logos/se-labs-logo.svg",
+                        redirectPath : "javascript:void(0)",
+                    },
+                    {
+                        awardTitle : "Leader in the Gigaom Radar Extended Detection and Response (XDR)",
+                        imageFile : "https://www.trellix.com/en-us/img/logos/gigaom-logo-dark.svg",
+                        redirectPath : "javascript:void(0)",
+                    },
+                    {
+                        awardTitle : "#1 Leading Solution with Gold Medal in the 2023 Info-Tech Data Quadrant for DLP",
+                        imageFile : "https://www.trellix.com/en-us/img/logos/gold-medal-trellix-dlp-endpoint-2023.png",
+                        redirectPath : "javascript:void(0)",
+                    },
+                ]
+            }
+        }
+    }
 }
 
 export default data;
