5c6eb32754842a97399f5222b0422b024012afd3
Bishal Biswas <bbiswas47@outlook.com>
2024-07-22 22:04:08 +0530

    utils error

--- src/pages/data/utils.js
+++ src/pages/data/utils.js
@@ -1,4 +1,4 @@
-function getFormattedDate(parameter_date:any){
+function getFormattedDate(parameter_date){
     const options = { day: 'numeric', month: 'long', year: 'numeric' };
     return parameter_date.toLocaleString('en-US', options)
 }
